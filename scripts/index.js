541f67974226268b9d668bdd533b326013b0360e
Denis <dennik1981@Gmail.com>
2022-09-16 18:32:05 +0300

    scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 import Swiper from "../lib/swiper-bundle.esm.browser.min.js";
 
+
+
 // simplebar
 new SimpleBar(document.querySelector(".country__list"), {
   classNames: {
@@ -67,7 +69,6 @@ formInput.forEach((input, i) => {
 });
 
 // currency
-
 const dataCurrency = {};
 
 const formatCurrency = (value, currency) => {
@@ -117,3 +118,46 @@ countryWrapper.addEventListener("click", ({ target }) => {
 		showPrice(target.dataset.currency);
   }
 });
+
+// scroll
+const headerNavigation = document.querySelector('.header__navigation'),
+  footerNavigation = document.querySelector('.footer__navigation');
+  
+const smothScroll = (trigger) => {
+  const SPEED = 0.3;
+  const scrolled = e => {
+    e.preventDefault();
+    const target = e.target;
+
+    if (target.matches('[href^="#"]')) {
+      let start = 0;
+      const pageY = window.pageYOffset;
+      const hash = target.getAttribute('href');
+
+      if (hash === '#') return;
+
+      const elem = document.querySelector(hash);
+      const coordinateElem = elem.getBoundingClientRect().top;
+      const allDistance = pageY + coordinateElem;
+      const scroll = time => {
+        if (!start) start = time;
+        const progress = time - start;
+        const r = (coordinateElem < 0 ?
+          Math.max(pageY - progress / SPEED, allDistance) :
+          Math.min(pageY + progress / SPEED, allDistance));
+
+        window.scrollTo(0, r);
+
+        const scrolling = coordinateElem < 0 ?
+          r > allDistance :
+          r < allDistance;
+        if (scrolling) requestAnimationFrame(scroll);
+      }
+      requestAnimationFrame(scroll)
+    }
+  }
+  trigger.addEventListener('click', scrolled);
+}
+
+smothScroll(headerNavigation)
+smothScroll(footerNavigation)
